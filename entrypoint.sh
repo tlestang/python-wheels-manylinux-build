54cbd61b5c0669b5441c409cf358b53067ef0c4f
Ralf Gabriels <ralfg@hotmail.be>
2020-02-28 15:19:15 +0100

    Add poetry support to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,13 @@ BUILD_REQUIREMENTS=$2
 SYSTEM_PACKAGES=$3
 PACKAGE_PATH=$4
 PIP_WHEEL_ARGS=$5
+BUILD_SYSTEM=$6
 
 if [ ! -z "$SYSTEM_PACKAGES" ]; then
     yum install -y ${SYSTEM_PACKAGES}  || { echo "Installing yum package(s) failed."; exit 1; }
 fi
+
+cd /github/workspace/"${PACKAGE_PATH}"
 
 # Compile wheels
 arrPY_VERSIONS=(${PY_VERSIONS// / })
@@ -18,19 +21,31 @@ for PY_VER in "${arrPY_VERSIONS[@]}"; do
     # Update pip
     /opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
 
-    # Check if requirements were passed
+    # Install build requirements, if passed
     if [ ! -z "$BUILD_REQUIREMENTS" ]; then
         /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir ${BUILD_REQUIREMENTS} || { echo "Installing requirements failed."; exit 1; }
     fi
-    
+
+    # Install poetry, if required
+    if [ "${BUILD_SYSTEM}" == "poetry" ]; then
+        /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir poetry || { echo "Installing poetry failed."; exit 1; }
+    fi
+
     # Build wheels
-    /opt/python/"${PY_VER}"/bin/pip wheel /github/workspace/"${PACKAGE_PATH}" -w /github/workspace/wheelhouse/ ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
+    if [ "${BUILD_SYSTEM}" == "pip" ]; then
+        /opt/python/"${PY_VER}"/bin/pip wheel . -w ./dist ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
+    elif [ "${BUILD_SYSTEM}" == "poetry" ]; then
+        /opt/python/"${PY_VER}"/bin/poetry install || { echo "Installing poetry package failed."; exit 1; }
+        /opt/python/"${PY_VER}"/bin/poetry build || { echo "Building wheels failed."; exit 1; }
+    else
+        echo "Invalid build-system: `${BUILD_SYSTEM}`, use `pip` or `poetry`."; exit 1;
+    fi
 done
 
 # Bundle external shared libraries into the wheels
-for whl in /github/workspace/wheelhouse/*-linux*.whl; do
-    auditwheel repair "$whl" --plat "${PLAT}" -w /github/workspace/wheelhouse/ || { echo "Repairing wheels failed."; auditwheel show "$whl"; exit 1; }
+for whl in ./dist/*-linux*.whl; do
+    auditwheel repair "$whl" --plat "${PLAT}" -w ./dist || { echo "Repairing wheels failed."; auditwheel show "$whl"; exit 1; }
 done
 
 echo "Succesfully build wheels:"
-ls /github/workspace/wheelhouse
+ls ./dist
